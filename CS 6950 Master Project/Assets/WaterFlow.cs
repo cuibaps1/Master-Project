394ff123989e27d5c2fe3ca08cc03fb1bb9ebdae
cuibaps1 <jadanngo@gmail.com>
2020-02-12 23:15:28 -0800

    creating physichelper class

--- CS 6950 Master Project/Assets/WaterFlow.cs
+++ CS 6950 Master Project/Assets/WaterFlow.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using Ditzelgames;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class WaterFlow : MonoBehaviour
 {
     //public properties
@@ -9,15 +12,75 @@ public class WaterFlow : MonoBehaviour
     public float WaterDrag = 10;
     public Transform[] FloatPoints;
 
+    //used components
+    protected Rigidbody Rigidbody;
+    protected Waves Waves;
+
+    //water line
+    protected float WaterLine;
+    protected Vector3[] WaterLinePoints;
+
+    //help Vectors
+    protected Vector3 smoothVectorRotation;
+    protected Vector3 TargetUp;
+    protected Vector3 centerOffset;
+
+    public Vector3 Center { get { return transform.position + centerOffset; } }
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        //get components
+        Waves = FindObjectOfType<Waves>();
+        Rigidbody = GetComponent<Rigidbody>();
+        Rigidbody.useGravity = false;
+
+        //compute center
+        WaterLinePoints = new Vector3[FloatPoints.Length];
+        for (int i = 0; i < FloatPoints.Length; i++)
+            WaterLinePoints[i] = FloatPoints[i].position;
+        centerOffset = PhysicsHelper.GetCenter(WaterLinePoints) - transform.position;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if (FloatPoints == null)
+            return;
+
+        for (int i = 0; i < FloatPoints.Length; i++)
+        {
+            if (FloatPoints[i] == null)
+                continue;
+
+            if (Waves != null)
+            {
+
+                //draw cube
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(WaterLinePoints[i], Vector3.one * 0.3f);
+            }
+
+            //draw sphere
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(FloatPoints[i].position, 0.1f);
+
+        }
+
+        //draw center
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(new Vector3(Center.x, WaterLine, Center.z), Vector3.one * 1f);
+            Gizmos.DrawRay(new Vector3(Center.x, WaterLine, Center.z), TargetUp * 1f);
+        }
     }
 }
