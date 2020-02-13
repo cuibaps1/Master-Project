7667362564e7a8bc1ec90d9060b6f486b2337c07
cuibaps1 <jadanngo@gmail.com>
2020-02-12 23:47:17 -0800

    creating WaterBoat script to control the boat

---
+++ CS 6950 Master Project/Assets/WaterBoat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBoat : MonoBehaviour
+{
+
+    //visible Properties
+    public Transform Motor;
+    public float SteerPower = 500f;
+    public float Power = 5f;
+    public float MaxSpeed = 10f;
+    public float Drag = 0.1f;
+
+    //used Components
+    protected Rigidbody Rigidbody;
+    protected Quaternion StartRotation;
+    protected ParticleSystem ParticleSystem;
+    protected Camera Camera;
+
+    //internal Properties
+    protected Vector3 CamVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
