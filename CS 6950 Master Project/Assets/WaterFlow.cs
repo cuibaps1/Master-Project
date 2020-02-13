6b208086c58b793346d77b205084c9ccb2fa034a
cuibaps1 <jadanngo@gmail.com>
2020-02-12 23:04:21 -0800

    Creating a cube object and make object water flow

---
+++ CS 6950 Master Project/Assets/WaterFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFlow : MonoBehaviour
+{
+    //public properties
+    public float AirDrag = 1;
+    public float WaterDrag = 10;
+    public Transform[] FloatPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
